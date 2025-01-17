e86b796d56cbae56f0e0376a45c3f5a056be2e88
Sumira <sumiragp@gmail.com>
2025-01-17 23:27:27 +0530

    feat: enhance HeroSection layout and styling, add Poppins font, and update Tailwind configuration

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,9 @@ export default function Home() {
     <main className="min-h-screen bg-gradient-to-br from-gray-900 via-gray-800 to-black text-white">
       <NavBar />
       <HeroSection />
-      <Projects />
-      <Skills />
-      <ContactUS />
+      {/* <Projects /> */}
+      {/* <Skills /> */}
+      {/* <ContactUS /> */}
 
       {/* Contact Section */}
     </main>
