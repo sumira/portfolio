8e6ada23864c3f1a78121a036d7c5764493244de
Sumira <sumiragp@gmail.com>
2025-01-20 00:10:07 +0530

    feat: add Contact Us section with form and social links

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import NavBar from "./components/nav-bar";
 import HeroSection from "./components/hero-section";
 import Projects from "./components/project-section";
 import Skills from "./components/skills-section";
-// import ContactUS from "./components/contact-us";
+import ContactUS from "./components/contact-us";
 
 export default function Home() {
   return (
@@ -11,9 +11,7 @@ export default function Home() {
       <HeroSection />
       <Projects />
       <Skills />
-      {/* <ContactUS /> */}
-
-      {/* Contact Section */}
+      <ContactUS />
     </main>
   );
 }
